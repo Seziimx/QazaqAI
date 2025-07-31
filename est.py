9b3eb74dbead95b14e19df330101b0b0c291bef6
Seziimx <baibolatova003@gmail.com>
2025-07-31 12:35:55 +0500

    Обновил проект: добавил Fireworks API поддержку

--- est.py
+++
@@ -1,9 +0,0 @@
-from fireworks import LLM
-
-llm = LLM(model="llama-v3-70b-instruct", deployment_type="auto")
-
-response = llm.chat.completions.create(
-    messages=[{"role": "user", "content": "Сәлем! Сен кімсің?"}]
-)
-
-print(response.choices[0].message.content)
